9bbed7eda5b6b7218fb5184560d4d1f08fb8f004
Shakeeb Ahmed <me@shakeebahmed.com>
2019-01-11 16:52:25 +0500

    Packages added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log( 'in' )
+import { createHooks } from '@wordpress/hooks'
+import { sprintf, _n } from '@wordpress/i18n';
+
+( function( $ ) {
+
+	'use strict'
+
+	window.rankMath = window.rankMath || {}
+	window.rankMath.hooks = createHooks()
+
+	function initializePostAnalysis() {
+		rankMath.hooks.doAction( 'rankMath.init' )
+	}
+
+	$( document ).ready( initializePostAnalysis )
+
+}( jQuery ) )
