e0ddc889c80e20525d128a1f7296cbcc60923955
balazsp <balazs.p@mythemeshop.com>
2020-09-20 21:22:46 +0200

    Revert "Filter for isInternalLink"
    This reverts commit 02f15bb795eca54ef17ff9e161c9caa7b971b696.

--- src/helpers/url.js
+++ src/helpers/url.js
@@ -4,11 +4,6 @@
 import urlMethods from 'url'
 import { includes } from 'lodash'
 
-/**
- * WordPress dependencies
- */
-import { applyFilters } from '@wordpress/hooks'
-
 const urlFromAnchorRegex = /href=(["'])([^"']+)\1/i
 
 /**
@@ -106,13 +101,6 @@ function couldBeDoFollow( url ) {
  * @return {boolean} Whether internal or not.
  */
 function isInternalLink( url, host ) {
-
-	// Short-circuit if filter returns non-null.
-	let filtered = applyFilters( 'rankMath_analysis_isInternalLink', null, url, host );
-	if ( filtered !== null ) {
-		return filtered
-	}
-
 	// Check if the URL starts with a single slash.
 	if ( ! includes( url, '//' ) && '/' === url[ 0 ] ) {
 		return true
