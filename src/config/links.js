42257bd52f8a1b12eecdf01d0292447db2563341
pratikrm <30603473+pratikrm@users.noreply.github.com>
2023-05-31 10:09:58 +0530

    Fixed: lint issue

--- src/config/links.js
+++ src/config/links.js
@@ -7,5 +7,5 @@ export default {
 	tocKbLink: 'https://rankmath.com/kb/score-100-in-tests/?utm_source=Plugin&utm_campaign=WP#table-of-contents',
 	contentLength: 'https://rankmath.com/kb/score-100-in-tests/?utm_source=Plugin&utm_campaign=WP#overall-content-length',
 	contentAILink: 'https://rankmath.com/kb/how-to-use-content-ai/',
-	productReview: 'https://rankmath.com/kb/woocommerce-product-schema/?utm_source=Plugin&utm_medium=Content%20Analysis%20Single%20Test%20KB&utm_campaign=WP#reviews'
+	productReview: 'https://rankmath.com/kb/woocommerce-product-schema/?utm_source=Plugin&utm_medium=Content%20Analysis%20Single%20Test%20KB&utm_campaign=WP#reviews',
 }
